87daf63cd61f2f53df280179a3aeaeee431af608
Ryan Mitchell <rtmitchell@google.com>
2019-10-10 08:15:55 -0700

    ysl: Use component-override to configure crosshatch Bluetooth
    Runtime Resource Overlays (RROs) can no longer change the value of
    resources in manifest when read during PackagerParser.
    
    This change uses component-override to enable the service by default
    on crosshatch devices.
    
    Remove usages of profile_supported_sap from overlays as they no
    longer configure the enable state of .sap.SapService.
    
    Bug: 135048762
    Test: adb shell pm query-services \
          com.android.bluetooth/.sap.SapService
    
    Change-Id: I89ba2aa61492f65b269849597282594507d2fdc3
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>
    Signed-off-by: ItsVixano <vixanonew@gmail.com>

--- device.mk
+++ device.mk
@@ -125,6 +125,10 @@ PRODUCT_PACKAGES += \
     libhwbinder \
     libhwbinder.vendor
 
+# Bluetooth
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
